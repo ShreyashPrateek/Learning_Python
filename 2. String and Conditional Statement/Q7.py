6132261aeb1122eb611b356802fb5244a3f4fb99
ShreyashPrateek <shreyashprateek01@gmail.com>
2025-11-26 15:18:55 +0530

    Divisible by 7 or not

---
+++ 2. String and Conditional Statement/Q7.py
@@ -0,0 +1,7 @@
+# WAP to check if the given number is divisible by 7 or not
+
+number = int(input("Enter a number: "))
+if number % 7 == 0:
+    print(number, "is divisible by 7.")
+else:
+    print(number, "is not divisible by 7.") 
