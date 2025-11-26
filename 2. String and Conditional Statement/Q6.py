f848e43690f7d0b698a9b223f9a360db3dd4cce0
ShreyashPrateek <shreyashprateek01@gmail.com>
2025-11-26 15:14:36 +0530

    Q: Greatest among 3 numbers

---
+++ 2. String and Conditional Statement/Q6.py
@@ -0,0 +1,17 @@
+# WAP to find the greateder among three numbers using nested if-else.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if num1 >= num2:
+    if num1 >= num3:
+        greatest = num1
+    else:
+        greatest = num3
+else:
+    if num2 >= num3:
+        greatest = num2
+    else:
+        greatest = num3 
+print("The greatest number is:", greatest)
