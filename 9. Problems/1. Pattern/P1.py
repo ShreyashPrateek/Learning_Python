d3d1935361fef115f56edba96377aa9037d6681a
ShreyashPrateek <shreyashprateek01@gmail.com>
2025-12-21 20:53:25 +0530

    Pattern Question

---
+++ 9. Problems/1. Pattern/P1.py
@@ -0,0 +1,11 @@
+''' Pattern Printing Problem 1
+***
+***
+***
+'''
+
+n = int(input("Enter the number of rows for the pattern: "))    
+for i in range(n):  # Outer loop for number of rows
+    for j in range(n):  # Inner loop for number of columns 
+        print("*", end="")  # Print star without newline
+    print()  # Move to the next line after printing one row
